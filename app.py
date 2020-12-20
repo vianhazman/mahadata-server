113f86a62aeef60435c7599470dc3e32e4471eaa
Favian Kharisma Hazman <favian.hazman@go-jek.com>
2020-12-20 23:22:52 +0700

    [fav] add download endpoints

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import operator
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 from bson.json_util import dumps
 from flask_caching import Cache
 from pymongo import MongoClient
@@ -17,6 +17,32 @@ Compress(app)
 CORS(app)
 client = MongoClient(environ.get("MONGO_CONNECTION_STRING"))
 db = client["testing"]
+
+def generate_time_series_csv(db_name):
+    collection = db[db_name]
+    cursor = collection.find({})
+    base='date,region,ratio,change\n'
+    for document in cursor:
+        date = document['date']
+        data = document['data']
+        transformed = []
+        for k,v in data.items():
+            row = "{},{},{},{}\n".format(date,k,v["ratio"],v["change"])
+            base += row
+    return base
+
+def generate_case_csv(db_name):
+    collection = db[db_name]
+    cursor = collection.find({})
+    base='date,region,new_cases\n'
+    for document in cursor:
+        date = document['date']
+        data = document['data']
+        transformed = []
+        for k,v in data.items():
+            row = "{},{},{}\n".format(date,k,v)
+            base += row
+    return base
 
 @app.route('/data/rank/<area_type>')
 @cache.cached(timeout=5000)
@@ -49,6 +75,33 @@ def province_daily_case():
     res = col.find({})
     return dumps(res)
 
+@app.route('/data/case/province/download/')
+def province_daily_case_download():
+    csv_str = generate_time_series_csv('movement_range_province')
+     return Response(
+        csv,
+        mimetype="text/csv",
+        headers={"Content-disposition":
+                 "attachment; filename=mobilitas_provinsi.csv"})
+
+
+@app.route('/data/daily/district/download/')
+def district_time_series_download():
+    csv_str = generate_time_series_csv('movement_range_district')
+     return Response(
+        csv,
+        mimetype="text/csv",
+        headers={"Content-disposition":
+                 "attachment; filename=mobilitas_kabkot.csv"})
+
+@app.route('/data/daily/province/download/')
+def province_time_series_download():
+    csv_str = generate_case_csv('historical_case')
+     return Response(
+        csv,
+        mimetype="text/csv",
+        headers={"Content-disposition":
+                 "attachment; filename=kasus_harian_provinsi.csv"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=environ.get("RUNNING_PORT"))
